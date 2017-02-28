1de92c61118372409fe2b097d1b5030b3bff9ad7
SPS Mac 2 <spsmac2@SPSs-Mac-mini.local>
2017-02-28 17:35:27 +0500

    agreement page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,48 @@
+
++ function($) {
+    'use strict';
+
+    // UPLOAD CLASS DEFINITION
+    // ======================
+
+    var dropZone = document.getElementById('drop-zone');
+    var uploadForm = document.getElementById('js-upload-form');
+
+    var startUpload = function(files) {
+        console.log(files)
+    }
+
+    uploadForm.addEventListener('submit', function(e) {
+        var uploadFiles = document.getElementById('js-upload-files').files;
+        e.preventDefault()
+
+        startUpload(uploadFiles)
+    })
+
+    dropZone.ondrop = function(e) {
+        e.preventDefault();
+        this.className = 'upload-drop-zone';
+
+        startUpload(e.dataTransfer.files)
+    }
+
+    dropZone.ondragover = function() {
+        this.className = 'upload-drop-zone drop';
+        return false;
+    }
+
+    dropZone.ondragleave = function() {
+        this.className = 'upload-drop-zone';
+        return false;
+    }
+
+}(jQuery);
+
+
+
+
+
+
 $(document).ready(function () {
     $('label.tree-toggler').click(function () {
         $(this).parent().children('ul.tree').toggle(500);
@@ -344,3 +389,11 @@ $( "#item" ).toggle(400);
 $(".anOtherButton").click(function() {
   $(".alldisable").attr('disabled', !$(".alldisable").attr('disabled'));
 });
+
+
+
+
+
+
+
+
