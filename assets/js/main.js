51e44e6a382a5ea2a9a37f61818129330eafcea8
Taimur Wali <taimurwali@gmail.com>
2017-03-14 14:06:41 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -358,3 +358,14 @@ $( "#avc" ).click(function() {
 $(".anOtherButton").click(function() {
     $(".alldisable").attr('disabled', !$(".alldisable").attr('disabled'));
 });
+
+
+
+function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
